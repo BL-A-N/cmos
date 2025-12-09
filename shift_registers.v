3fabb0df642272d33d8375a798ed78cc6a1d156b
Anirudh R <140381948+BL-A-N@users.noreply.github.com>
2025-12-09 06:12:36 +0530

    Update shift_registers.v

--- shift_registers.v
+++ shift_registers.v
@@ -16,7 +16,7 @@ module asyn_shift(Dout,Din,clk,rst,SI,shift,ld);
   output reg [7:0] Dout;
   input [7:0] Din;
   input clk,rst,ld,SI,shift;
-  always @(posedge clk or rst) begin 
+  always @(posedge clk or negedge rst) begin 
     if (rst)
       Dout <= 8'b0;
     else if (ld)
